d02df7e56663e4295a3a621e8ac1d7bce97a90a0
leba <qayinhabel@gmail.com>
2018-02-25 12:31:58 +0100

    Testing with screen.h and test.c files

--- screen.h
+++ screen.h
@@ -1,4 +1,22 @@
 /*
-Plots texture of object to the screen starting at given x and y coordinates.
-*/
+ * SCREEN.H -ALWAYS IN PLAY- COMPONENT
+ */
+
+//SCREEN DIMESIONS
+#define HEIGHT 420 
+#define WIDTH 480
+
+/**
+ *Plots texture of object to the screen starting at given x and y coordinates.
+ *@params X int. Left Horizontal position.
+ *@params y int. Top Vertical position.
+ *@params *obj struct object. Pointer to an object.
+ */
 void plotTexture(int x, int y, struct object *obj);
+
+/**
+ *Plots useful information to the player. We'll use console and printfs for 
+ *starters to later implement ingame window texts.
+ *@params msg char[]. The string containing the message.
+ */
+void plotText(char[] msg);
