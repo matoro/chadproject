ca542c8df3b89ec3b0290dd25366dc1def21fba8
leba <qayinhabel@gmail.com>
2018-03-04 13:07:52 +0100

    New testing. Separation of modules

---
+++ object.h
@@ -0,0 +1,98 @@
+/*
+ *OBJECT.H FILE: BASIC STRUCT DATA COMMON FOR ALL OBJECTS.
+ */
+#include "screen.h" //DO I NEED TO INCLUDE SDL_PLOTTER.h?
+
+struct texture{
+	//DEFAULTS TO WHITE.
+	int red = 255;
+	int green = 255;
+	int blue = 255;
+};
+
+struct size{	
+	
+	int alto = 0;
+	int ancho = 0;
+};
+
+struct position {
+	
+	int x;
+	int y;
+	int direction; //Pointing at. 1-360.
+};
+
+struct object{
+
+	struct texture 	textureObj;
+	struct size 	sizeObj;
+	struct position posObj;
+};
+
+
+void setTexture(struct* object obj,int r, int g, int b){
+	
+	obj->textureObj.red   = r;
+	obj->textureObj.green = g;
+	obj->textureObj.blue  = b;
+}
+
+
+int[] getTexture(struct* object obj){
+
+	int clr[3];
+	clr[0] = obj->textureObj.red;
+	clr[1] = obj->textureObj.green;
+	clr[2] = obj->textureObj.blue;
+
+	return clr;
+}
+
+
+void setSize(struct* object  obj,int height, int width){
+	
+	obj->sizeObj.alto   = height;
+	obj->sizeObj.ancho  = width;
+}
+
+int[] getSize(struct* object obj){
+
+	int dim[2];
+	dim[0] = obj->sizeObj.ancho;
+	dim[1] = obj->sizeObj.alto;
+	
+	return dim;
+}
+
+void setPosition(struct* object obj,int i, int j,int dir){
+
+	obj->posObj.x  = i;
+	obj->posObj.y  = j;
+	obj->posObj.direction = dir;
+}
+
+int[] getPosition(struct* object obj,int j, int i,int dir){
+	
+	int pos[3];
+	pos[0] = obj->posObj.y;
+	pos[1] = obj->posObj.x;
+	pos[2] = obj->posObj.direction;
+
+	return pos;
+}
+
+void plotObject(struct* object obj,SDL_Plotter plotter){
+	
+	int size[2] = getSize(obj);
+	int pos[3]  = getPosition(obj);
+	int clr[3]  = getTexture(obj);
+
+	for(int i=0; i<size[0];i++){
+		for(int j=0;j<size[1];j++){
+			plotter.plotPixel(i+pos[0],j+pos[1]
+				,clr[0],clr[1],clr[2]);
+		}
+	}
+}
+
---
+++ player.h
@@ -0,0 +1,63 @@
+/*
+ *PLAYER.H FILE: CONSTAINS BASIC INFO AND FUNCTIONS ABOUT THE PLAYERs OBJ.
+ */
+
+#include "object.h"
+#include "screen.h"//Do I need to include this if object already includes screen?
+#include <stdbool.h>
+
+typedef struct PlayerObj{ //Do I need a structTag?
+
+	struct object obj;
+	int health = 100;
+	int ammo   = 100;
+	//...	
+}PlayerObj;
+
+void setHealth(PlayerObj* player,int hp){
+	
+	player->health = hp;
+}
+
+int getHealth(PlayerObj* player){
+
+	return player->health; 
+}
+
+void setAmmo(PlayerObj* player,int munition){
+	
+	player->health = munition;
+}
+
+int getAmmo(PlayerObj* player){
+
+	return player->ammo; 
+}
+
+bool isAlive(PlayerObj* player){
+	
+	if ((player->health)>0)	return true;
+	return false;
+}
+
+bool hasAmmo(PlayerObj* player){
+	if ((player->ammo)>0) return true;
+	return false;
+}
+
+void plotPlayer(PlayerObj,SDL_Plotter plot){
+	
+	plotObject(&PlayerObj.obj,plot);
+}
+
+
+PlayerObj createPlayer(int h, int w, int j, int i, int dir){
+	
+	PlayerObj a;
+
+	setTexture(&a.obj,255,99,71); 	//TOMATO RED
+	setSize(&a.obj,h,w);		//DIMENSION
+	setPosition(&a.obj,j,i,dir);	//POSITION
+
+	return a;
+}
