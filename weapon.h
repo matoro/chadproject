683e60d07fe6686079485daeebfcf7138352a716
leba <qayinhabel@gmail.com>
2018-03-17 19:14:43 +0100

    Added weapon.h file

---
+++ weapon.h
@@ -0,0 +1,19 @@
+/*
+ * WEAPON.H -VISIBELE IN PLAY- COMPONENT
+ * @author leba39, mortonmann.
+ */
+#ifndef WEAPON_H_INCLUDED
+#define WEAPON_H_INCLUDED
+
+/*
+ * Enum weapon. All available types of weapons in the game.
+ * Their value reflects their damage.
+ */
+enum weapon{
+
+	gun 		= 20;
+	shotgun 	= 40;
+	machinegun 	= 60;
+	rifle 		= 80;
+};
+#endif
