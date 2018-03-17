3f7d4091eb1314b0216cbb286fc34b07e87ff3da
leba <qayinhabel@gmail.com>
2018-03-17 17:45:53 +0100

    Changed rate in horizontal movements to simulate roughness.

--- object.c
+++ object.c
@@ -91,12 +91,14 @@ struct position changePosition(struct object *obj,char movement, int rate){
 			fprintf(stdout,"You pressed D: %d\n",deltaDir);
 			break;
 		case 'Q':
-			//Same as going forward in (dir-90)º
+			//Same as going forward in (dir-90)º.Half the rate to simulate a slow hard movement.
+			rate/=2;
 			deltaX = (int) round(rate*sin((pos.direction-90)*M_PI/180.0));
 			deltaY = (int) round(-rate*cos((pos.direction-90)*M_PI/180.0));
 			deltaDir = 0;
 			break;
 		case 'E':
+			rate/=2;
 			deltaX = (int) round(rate*sin((pos.direction+90)*M_PI/180.0));
 			deltaY = (int) round(-rate*cos((pos.direction+90)*M_PI/180.0));
 			deltaDir = 0;
