3583ddf7ad5f44d47403edb897481bdcf441ce70
leba <qayinhabel@gmail.com>
2018-03-29 19:29:53 +0200

    colored enemy

--- enemy.c
+++ enemy.c
@@ -73,14 +73,41 @@ void plotEnemy(struct EnemyObj enemy,SDL_Plotter* plot){
 struct EnemyObj createEnemy(struct size dim, struct position pos){
 	
 	struct EnemyObj e;
-	struct texture enemytexture; 	//BLACK
-	
-	enemytexture.red  = 0;
-	enemytexture.green = 0; 
-	enemytexture.blue  = 0;
-	setTexture(&e.obj, enemytexture);
-	setSize(&e.obj,dim);		//DIMENSION
-	setPosition(&e.obj,pos);	//POSITION
+	setSize(&e.obj,dim);            //DIMENSION
+        setPosition(&e.obj,pos);        //POSITION
 
+        struct texture skinColor;
+        skinColor.red   = 255;
+        skinColor.green = 210;
+        skinColor.blue  = 160;
+
+        struct texture darkHair;	
+        darkHair.red   = 5;
+        darkHair.green = 5;
+        darkHair.blue  = 5;
+
+        struct texture eyeColor;	//RED for enemies.
+        eyeColor.red   = 180;
+        eyeColor.green = 5;
+        eyeColor.blue  = 5;
+
+	int totalPixel = dim.alto * dim.ancho;
+        e.obj.textureObj = (struct texture*)malloc(sizeof(struct texture)*totalPixel);
+	
+	//WE PAINT THE OBJECT
+
+
+	int count = 0;
+        for(int i = 0;i<dim.ancho;i++){
+                for(int j = 0;j<dim.alto;j++){
+                        setTexture(&e.obj,(i<=(dim.alto)&&j<=(dim.ancho/2)) ? skinColor:darkHair,count);        //IN-HALF
+                        if(i<=(dim.alto)&&j==(dim.ancho/4)){                                                    //HORIZONTAL QUARTER
+                                if(abs(dim.alto/2-i)>dim.alto/8)        setTexture(&e.obj,eyeColor,count);      //AVOID CENTER
+                        }
+
+                        count++;
+                }
+        }
+	
 	return e;
 }
